48c6f22cbb9835bfb851728cde32ee0e61ec61ce
Rabi Prasad <rabiprasad607@gmail.com>
2022-06-16 04:38:55 +0530

    firebase-project and firebase initialized

---
+++ firebase-project/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { db, colRef } from "./firebase";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Learning Firebase 😁</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase-project/src/firebase.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+console.log("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKqNYklIYXcfHJYE1r0J-qzNFDWF6uRWk",
+  authDomain: "fir-practice-57ef0.firebaseapp.com",
+  projectId: "fir-practice-57ef0",
+  storageBucket: "fir-practice-57ef0.appspot.com",
+  messagingSenderId: "926773302529",
+  appId: "1:926773302529:web:0860cca257d99ccdc5f700",
+  measurementId: "G-SP9G6WV71Y",
+};
+
+// initializing app
+initializeApp(firebaseConfig);
+
+// initializing database
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "Books");
+
+// get data
+getDocs(colRef)
+  .then((snapshot) => {
+    const docs = snapshot.docs;
+    const books = [];
+    docs.forEach((doc) => books.push({ ...doc.data(), id: doc.id }));
+    console.log(books);
+  })
+  .catch((err) => console.log(err));
+
+export { db, colRef };
