44a30666c51acd615491a2633dc066910013aa99
Rabi Prasad <rabiprasad607@gmail.com>
2022-07-09 04:10:25 +0530

    feat: add/delete data in firebase database

--- firebase-project/src/App.js
+++ firebase-project/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { db, colRef } from "./firebase";
+import ModifyData from "./ModifyData";
 
 function App() {
   return (
     <div className="App">
       <h2>Learning Firebase 😁</h2>
+      <ModifyData />
     </div>
   );
 }
